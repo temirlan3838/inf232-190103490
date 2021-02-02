2d6f54b86d15cc8cc70a95544639214cc01ae94a
Temirlan Kadyr <190103490@stu.sdu.edu.kz>
2021-02-02 19:33:09 +0600

    2nd commit

---
+++ file1.php
@@ -0,0 +1,11 @@
+<?php
+$num = 123456;
+$sum=0;
+$rem=0;
+  for ($i = 0; $i <= strlen($num); $i++) {
+  $rem = $num % 10;
+   $sum = $sum + $rem;
+   $num = $num / 10;
+  }
+ echo "Sum of digits 123456 is $sum";
+ ?>
